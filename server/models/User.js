8c16a5d1cdc853148278738fb0fde2edd877066e
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-22 19:36:52 +0530

    create user schema

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+//require
+const mongoose=require("mongoose");
+
+//User schema
+const UserSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    account:{
+        type:String,
+        enum:["Admin" ,"Student" , "Instructor"],
+        required:true,
+    },
+    additionalDetails:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Profile",
+    },
+    course:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Courses",
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    courseProgress:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"CourseProgress"
+        }
+    ],
+});
+
+module.exports=mongoose.model("User" , UserSchema);
