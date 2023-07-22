124bd6eb9eb0df8c19b38480905e8d76bb6aae53
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-22 21:27:36 +0530

    create Course schema

---
+++ server/models/Course.js
@@ -0,0 +1,58 @@
+//require
+const mongoose = require('mongoose')
+
+//course schema
+const CourseSchema = new mongoose.Schema({
+	courseName:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    courseDescription:{
+        type:String,
+        trim:true,
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    whatYouWillLearn:{
+        type:String,
+        trim:true,
+    },
+    CourseContent:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section",
+        }
+    ],
+    ratingAndReviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"RatingAndReview",
+        }
+    ],
+    price:{
+        type:Number,
+        required:true,
+    },
+    thumbnail:{
+        type:String,
+    },
+    tag:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Tag",
+            }
+    ],
+    studensEnrolled:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        }
+    ]
+})
+
+module.exports = mongoose.model('Course', CourseSchema)
