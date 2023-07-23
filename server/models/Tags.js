17c03f5aae201e74e85f1b706f728a4e205e9faa
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-23 18:19:30 +0530

    edit

--- server/models/Tags.js
+++ server/models/Tags.js
@@ -3,18 +3,20 @@ const mongoose = require('mongoose')
 
 //Tag schema
 const TagSchema = new mongoose.Schema({
-	name:{
-        required:true,
-        type:String
-    },
-    description:{
-        type:String,
-        trim:true
-    },
-    course:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Course"
-    }
+	name: {
+		required: true,
+		type: String,
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	course: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Course',
+		},
+	],
 })
 
 module.exports = mongoose.model('Tags', TagSchema)
