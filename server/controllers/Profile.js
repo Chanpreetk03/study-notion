dd0ce27c67b5fb6ca613c6c266d7f3489b3e0460
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-24 19:24:33 +0530

    create delete user profile handler

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -47,3 +47,39 @@ exports.updateProfile = async (req, res) => {
 }
 
 //schedule a request:account delete after 3 days etc
+exports.deleteAccount = async(req,res) =>{
+    try {
+        //fetch data
+        const id=req.user.id;
+
+        //validate
+        const userDetails=await User.findById(id);
+        if(!userDetails){
+            return res.status(400).json({
+				success: false,
+				message: 'User no found',
+			})
+        }
+
+        //delete profile of user 
+        await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
+
+        //delete it from course too (number of students lessened)
+        // then delete user
+        await User.findByIdAndDelete({_id:id});
+        
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"user deleted successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Account could not be deleted"
+        })
+    }
+}
+
+//what is a chronjob
