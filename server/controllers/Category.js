ae63f5a23c5063ea3a8088780f523daa791debba
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-25 17:22:58 +0530

    create getCategory controller

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -47,8 +47,50 @@ exports.showAllCategories = async (req, res) => {
 			message: 'all tags return successfully',
 			allCategories,
 		})
-        
 	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+	}
+}
+
+//categoryPageDetails
+exports.getCategoryPageDetails = async (req, res) => {
+	try {
+		//fetch category id
+		const { categoryId } = req.body
+
+		//get courses for specified categoryid
+		const selectedCategory = await Category.findById(categoryId).populate('courses').exec()
+
+		//validation
+		if (!selectedCategory) {
+			return res.status(404).json({
+				success: false,
+				message: 'data not found',
+			})
+		}
+
+		//get courses for different categories
+		const differentCategories = await Category.find({
+			_id: { $ne: categoryId },
+		})
+			.populate('courses')
+			.exec()
+
+		//get top selling courses
+
+		//return response
+		return res.status(200).json({
+			success: true,
+			data: {
+				selectedCategory,
+				differentCategories,
+			},
+		})
+	} catch (error) {
+		console.log(error)
 		return res.status(500).json({
 			success: false,
 			message: error.message,
