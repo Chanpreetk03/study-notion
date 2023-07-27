ac20c13806fecb2c5eadff251c9d0a8a53c3c571
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-27 18:04:14 +0530

    section 1 home page

---
+++ src/components/Core/HomePage/CodeBlocks.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CTAButton from "./Button";
+import HighlightText from "./HighlightText";
+import { FaArrowRight } from "react-icons/fa";
+
+const CodeBlocks = ({
+    position, heading,subheading,ctabtn1,ctabtn2,codeblock,backgroundGradient,codeColor
+})=>{
+    return(
+        <div className={`flex ${position} my-20 justify-between gap-10`}>
+
+        </div>
+    )
+};
+
+export default CodeBlocks
---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {FaArrowRight} from "react-icons/fa";
+import HighlightText from "../components/Core/HomePage/HighlightText";
+import CTAButton from "../components/Core/HomePage/Button";
+import Banner from "../assets/Images/banner.mp4"
+
+const Home = () =>{
+    return(
+        <div>
+            {/* section 1 */}
+            <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-center max-w-maxContent">
+
+                <Link to={"/signup"}>
+
+                    <div className="group mx-auto rounded-full ring-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit mt-16 p-1">
+
+                        <div className="flex flex-row items-center rounded-full px-10 py-[15px] transition-all duration-200 group-hover:richblack-900">
+
+                            <p>Become An Instructor</p>
+
+                            <FaArrowRight/>
+
+                        </div>
+                    </div>
+                </Link>
+
+                <div className="text-center font-semibold text-4xl mt-7">
+                    Empower Your Future with 
+                    <HighlightText text={"Coding Skills"}/>
+                </div>
+
+                <div className="w-[90%] text-center text-lg font-bold text-richblack-300">
+                    With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on-project, quizzes, and personalized feedback from instructors
+                </div>
+                <div className="flex flex-row gap-7 mt-8">
+                    <CTAButton active={true} linkto={"/signup"}>
+                        Learn More
+                    </CTAButton>
+                    <CTAButton active={false} linkto={"/login"}>
+                        Learn More
+                    </CTAButton>
+                </div>
+            </div>
+
+            <div className="shadow-blue-200 mx--3 my-14">
+                <video muted loop autoPlay>
+                <source src={Banner} type="video/mp4" />
+                </video>
+            </div>
+
+            {/* Code section 1 */}
+            <div>
+                <CodeBlocks/>
+            </div>
+
+            {/* section 2 */}
+
+            
+
+            {/* section 3 */}
+            {/* Footer */}
+        </div>
+    )
+}
+
+export default Home;
