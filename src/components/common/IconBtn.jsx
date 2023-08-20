83aac8688f9fa119bd09392c6181e995f683a81b
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-08-20 20:54:48 +0530

    debug

--- src/components/common/IconBtn.jsx
+++ src/components/common/IconBtn.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function IconBtn({
 	text,
 	onClick,
@@ -10,7 +12,7 @@ export default function IconBtn({
 	return (
 		<button
 			disabled={disabled}
-			onClick={onclick}
+			onClick={onClick}
 			className={`flex items-center ${
 				outline ? 'border border-yellow-50 bg-transparent' : 'bg-yellow-50'
 			} cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 ${customClasses}`}
